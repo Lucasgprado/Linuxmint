abb36027a7f9347eff52a099dfebabf36a45ee80
Lucasgprado <53831952+Lucasgprado@users.noreply.github.com>
2019-08-06 15:20:48 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,27 @@
-Lucas Gomes
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+Atalhos para o terminal
+Ctrl+alt+t
+Crtl+alt+f1 (terminal puro)
+para voltar crtl+alt+f7
 
+# =super usuario
+$ =usuario comum
+~ =Seu usuario
+
+ls =listar pastas
+ls -lh =listar arquivos do diretorio
+
+ctrl+l =limpar tela
+
+cd / =raiz do linux
+cd - =voltar ao diretorio pessoal)
+
+azul escuro=diretorio
+azul claro=atalho
+cinza=arquivos
+marca texto verde=perigo
+
+pwd =mostrar qual diretorio esta
+
+mkdir =criação dos diretorios 
+
+TAB =preencher comando
